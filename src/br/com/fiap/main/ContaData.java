57d7c05d1eb13b0e3828e81a5acec54d2e84d6ff
glMuller <glmuller2001@gmail.com>
2025-04-26 19:37:30 -0300

    captando e guardando no "Period" as datas digitadas pelo usuário

--- src/br/com/fiap/main/ContaData.java
+++ src/br/com/fiap/main/ContaData.java
@@ -1,7 +1,13 @@
 package br.com.fiap.main;
 
+import javax.swing.*;
+import java.time.Period;
+
 public class ContaData {
     public static void main(String[] args) {
+        Period dataInicial = Period.parse(JOptionPane.showInputDialog("Digite uma data inicial:"));
+        Period dataFinal = Period.parse(JOptionPane.showInputDialog("Digite uma data final:"));
+        JOptionPane.showMessageDialog(null,"Data inicial: ");
 
     }
 }

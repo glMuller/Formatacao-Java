a4e2774b350d071ad26db4670f73c664fdd75685
glMuller <glmuller2001@gmail.com>
2025-04-26 20:20:08 -0300

    Programa finalizado.

--- src/br/com/fiap/main/ContaData.java
+++ src/br/com/fiap/main/ContaData.java
@@ -1,13 +1,29 @@
 package br.com.fiap.main;
 
 import javax.swing.*;
+import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class ContaData {
     public static void main(String[] args) {
-        Period dataInicial = Period.parse(JOptionPane.showInputDialog("Digite uma data inicial:"));
-        Period dataFinal = Period.parse(JOptionPane.showInputDialog("Digite uma data final:"));
-        JOptionPane.showMessageDialog(null,"Data inicial: ");
+        // Definir o formato de data que o usuário vai usar
+        DateTimeFormatter formatado = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+       //Receber data Inicial
+        String dataInicialString = JOptionPane.showInputDialog("Digite uma data inicial:");
+        LocalDate dataInicial = LocalDate.parse(dataInicialString, formatado);
+
+        //Receber data final
+        String dataFinalString = JOptionPane.showInputDialog("Digite uma data final");
+        LocalDate dataFinal = LocalDate.parse(dataFinalString, formatado);
+
+
+        //Usar Period pra descobrir o período entre as datas
+        Period periodo = Period.between(dataInicial,dataFinal);
+
+        //Mostrar ao usuário
+        JOptionPane.showMessageDialog(null,"Data inicial: " + dataInicial.format(formatado) + "\nData final: " + dataFinal.format(formatado) + "\nEntre as datas fornecidas existem: " + periodo.getYears() + "ano(s), " + periodo.getMonths() + "Mês(es) e " + periodo.getDays() + "Dia(s)");
 
     }
 }
